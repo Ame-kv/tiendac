15132e7f064333ad8a6b77aad0e087a6a55923c0
@manuel.martinez <@manuel.martinez>
2025-07-18 19:18:27 -0600

    avec plaisir <3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(dirname(__DIR__))
     ->withRouting(
-        __DIR__.'/../routes/web.php',
-        __DIR__.'/../routes/console.php',
-        '/up',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(["admin" => AdminMiddleware::class]);
