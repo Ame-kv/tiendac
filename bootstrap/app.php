59b3df0edfdf535368b5544cf02388cbcd3701b8
Ame-kv <amelyku0204@gmail.com>
2025-07-21 23:24:41 -0600

    Resuelto conflicto en bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(dirname(__DIR__))
-<<<<<<< HEAD
-    ->withRouting(function () {
-        require __DIR__ . '/../routes/web.php';
-        require __DIR__ . '/../routes/console.php';
-=======
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -18,7 +13,6 @@ return Application::configure(dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(["admin" => AdminMiddleware::class]);
->>>>>>> 15132e7f064333ad8a6b77aad0e087a6a55923c0
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
